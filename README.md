779307c5284bf06c9d0949aae5f7da036185a628
anton-zamnyborshch <anton.zamniborshch@theopentag.com>
2022-10-04 18:33:46 +0200

    Add simple cache for templates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Golang Apps for practice
 
 ### Basic web Application
-1. go run main.go
+1. `go run .`
 2. http://localhost:8081/
 
 ### Golang lessons
--- basic_web_application/pkg/template_renderer.go
+++ basic_web_application/pkg/template_renderer.go
@@ -6,19 +6,38 @@ import (
 	"net/http"
 )
 
+var templateCache = make(map[string]*template.Template)
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles(
-		"./templates/"+tmpl,
-		"./templates/base.layout.tmpl",
-	)
+	t, err := getOrCreateCachedTemplate(tmpl)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	err = parsedTemplate.Execute(w, nil)
+	err = t.Execute(w, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+}
+
+func getOrCreateCachedTemplate(tmpl string) (*template.Template, error) {
+	t, inCache := templateCache[tmpl]
+	if inCache {
+		return t, nil
+	}
+
+	files := []string{
+		"./templates/" + tmpl,
+		"./templates/base.layout.tmpl",
+	}
+
+	parsedTemplate, err := template.ParseFiles(files...)
+	if err != nil {
+		return nil, err
+	}
+
+	templateCache[tmpl] = parsedTemplate
+	return parsedTemplate, nil
 }
